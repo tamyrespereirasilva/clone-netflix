7eeed3f85c4bf0d101a40c4e609fa5d5f899edc7
tamyrespereirasilva <tamyrestn88@gmail.com>
2022-12-20 19:53:31 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "f3f4997457b3ff11e906d21b90d4350e"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express") 
+const path = require("path")
+const port = 3333
+
+let initalPath = path.join(__dirname, "public")
+
+let app = express()
+app.use(express.static(initalPath))
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(initalPath, "index.html"))
+})
+
+app.listen(port, ()=>{
+    console.log(`Server start up on port ${port}!`)
+})
